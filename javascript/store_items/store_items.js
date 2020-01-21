cde5d33032d6fea66168550972cebf0b24599f33
john <johnnonso090@gmail.com>
2020-01-20 17:09:07 -0800

    created the search alogrithm

--- javascript/store_items/store_items.js
+++ javascript/store_items/store_items.js
@@ -1,6 +1,32 @@
 console.log('started ')
 
 
+function searchForItem() {
+	// body...
+
+	
+ 	var input, filter, ul, cont, span, i, txtValue;
+
+    input = document.querySelector("#search-bar");
+    filter = input.value.toUpperCase();
+    
+    cont =  document.querySelectorAll(".item-cont");
+     
+     
+    for (i = 0; i < cont.length; i++) {
+        span = cont[i].children[0].children[1].children[0];
+        console.log(span.textContent)
+        txtValue = span.textContent || span.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            cont[i].style.display = "";
+        } else {
+            cont[i].style.display = "none";
+        }
+    }
+    console.log("work")
+
+}
+
 
 
 function myF(argument) {
